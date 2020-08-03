27b0fac6d5e1c46610ecb5f1d84e65217b85ccfe
Vaishnaw, Atishubh [GTSUS NON-J&J] <avaishna@its.jnj.com>
2020-08-03 14:52:41 +0530

    setting changed

--- src/settings.ts
+++ src/settings.ts
@@ -3,8 +3,10 @@ var settings = {
     testing: {
         enableWebTests: true,
         sp: {
-            id: "e3eaf46c-417d-4a08-a320-18ebf62e5f09",
-            secret: "fRbO5eb18LUv+4T4zqKJKuSyWImiHuNW4pLV8MYbE8k=",
+            // id: "e3eaf46c-417d-4a08-a320-18ebf62e5f09",
+            id: "1603a486-7ce4-4441-a34e-c1f64cb6462a",
+            // secret: "fRbO5eb18LUv+4T4zqKJKuSyWImiHuNW4pLV8MYbE8k=",
+            secret: "Z6OMhjHSE63y4sHUEpU6AriZtOl5Bfv0F+EH4K+qyhc=",
             url: "https://chinhaar.sharepoint.com/sites/developer",
             notificationUrl: "",
         },
